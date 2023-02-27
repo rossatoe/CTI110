7c805ac7380eba09f6fa329f175c04a2e94c527c
rossatoe <124811784+rossatoe@users.noreply.github.com>
2023-02-27 13:38:45 -0500

    Update P2HW1_rossatoEzequiel.py

--- P2HW1_rossatoEzequiel.py
+++ P2HW1_rossatoEzequiel.py
@@ -1,4 +1,30 @@
-budget = float(input('Enter Budget:\n'))
+# This program Shows all the expensives of your travel
+# 2/27/2023
+# CTI-110 P2HW1 - Travel
+# Ezequiel Rossato
+#
+# Display "Enter budget:"
+# Input budget
+# Display "Where are you traveling?"
+# Input dest
+# Display "How much the gas cost?"
+# Input gas
+# Display "How much the accomodation cost?"
+# Input accom
+# Display "How much the food cost?"
+# Input food
+# Calculate the expenses
+# Calculate the remaining balance
+# Display "------------Travel Expenses------------"
+# Display "Location:", dest
+# Display "Initial Budget: ", budget
+# Display "Fuel: ", gas
+# Display "Accomodation:", accom
+# Display "Food:, food
+# Display "---------------------------------------"
+# Display "Remaining Balance:", result
+#
+budget = float(input('Enter budget:\n'))
 dest = input('Where are you traveling?\n')
 gas = float(input('How much the gas cost?\n'))
 accom = float(input('How much the accomodation cost?\n'))
