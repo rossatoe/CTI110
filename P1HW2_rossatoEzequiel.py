bc745c3d453d8535c6099cf10b1090b53e9356cc
rossatoe <124811784+rossatoe@users.noreply.github.com>
2023-02-27 11:43:42 -0500

    Update and rename P1HW2_rossatoe.py to P1HW2_rossatoEzequiel.py

--- P1HW2_rossatoEzequiel.py
+++ P1HW2_rossatoEzequiel.py
@@ -3,6 +3,20 @@
 # CTI-110 P1HW2 - Travel Expense
 # Ezequiel Rossao
 #
+# Display "Enter your Budget for this trip:"
+# Input budget
+# Display "Where are you traveling?"
+# Input dest
+# Display "How much the gas cost?"
+# Input gas
+# Display "How much the accomodation cost?"
+# Input accom
+# Display "How much the food cost?"
+# Input food
+# sum the expenses
+# substract the expenses from the budget
+# Display "You have", result, "left on your budget after your trip to", dest
+#
 budget = float(input('Enter your Budget for this trip:\n'))
 dest = input('Where are you traveling?\n')
 gas = float(input('How much the gas cost?\n'))
