7d10183db8bb3325747649c34b70a2d0043d0331
rossatoe <124811784+rossatoe@users.noreply.github.com>
2023-04-19 11:55:27 -0400

    Update P4HW1_RossatoEzequiel.py

--- P4HW1_RossatoEzequiel.py
+++ P4HW1_RossatoEzequiel.py
@@ -5,20 +5,25 @@
 #
 scoreAmnt = int(input("How many scores do you want to enter? "))
 count = 0
+scoreList = [] 
 for count in range(scoreAmnt):
     count +=1
-    Score = int(input(f'Enter score # {count} : '))
+    Score = float(input(f'Enter score # {count} : '))
     if Score >= 0 and Score <= 100:
         continue
     else:
         print("INVALID Score entered!!!!")
         print("Score should be between 0 and 100")
-        Score = int (input(f'Enter score # {count} again: '))
-    scoreList[count] = Score
+        Score = float(input(f'Enter score # {count} again: '))
+    scoreList.append(Score)
         
 print("------------Results------------")
 lowest = min(scoreList)
-avrg = scoreList / len(scoreList)
+
+scoreList2 = scoreList
+scoreList2.remove(lowest)
+
+avrg = sum(scoreList2) / len(scoreList2)
 if avrg >= 90:
     grade = 'A'
 elif avrg >= 80:
@@ -27,5 +32,13 @@ elif avrg >= 70:
     grade = 'C'
 else:
     grade = 'D'
-print(f'{"Lowest Score : ":<20}{lowest}')
-print(f'{"Lowest Score : ":<20}{lowest}')
+print(float(f'{"Lowest Score : ":<20}{lowest}'))
+count2 = 0
+print(float(f'{"Modified List : ":<20} + end=' ''))
+for count2 in range(scoreAmnt):
+    count2 += 1
+    print(float(f'{scoreList[count2]} + end=' ''))
+print()
+print(float(f'{"Score Average: ":<20}{avrg}'))
+print(f'{"Grade: ":<20}{grade}')
+print("-------------------------------")
